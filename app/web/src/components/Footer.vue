0ff4f7454546f91b4669d745761c4a32d885acc9
Adrián Del Rincón <adrl1988@gmail.com>
2019-09-11 10:39:39 +0200

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-      Sagunto  ENCATEAM 2019
+      Puerto de Sagunto ENCATEAM 2019
       </div>
       <div class="col-lg-6">
       </div>
